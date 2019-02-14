ba47d9108c8ca4a317ad76db35cbc249106e448f
Tab/jy <admin@tabjy.com>
2019-02-13 19:24:53 -0800

    init GitHub pages

---
+++ 082f4e3be24378e7474528411ec6a8e6.glsl
@@ -0,0 +1,8 @@
+precision mediump float;
+#define GLSLIFY 1
+
+attribute vec4 vPosition;
+
+void main() {
+    gl_Position = vPosition;
+}
---
+++ 396d3aa6b65b8f5db84bf4b5b2bb5e0b.glsl
@@ -0,0 +1,8 @@
+precision mediump float;
+#define GLSLIFY 1
+
+uniform vec4 fColor;
+
+void main() {
+    gl_FragColor = fColor;
+}
